745983b747aecd2e7bbc726b4f9e008d7b3cb2a7
Bobby R. Ward <bobbyrward@gmail.com>
2020-07-20 17:34:50 -0500

    Switch to using a PAT for checkouts

--- dist/index.js
+++ dist/index.js
@@ -40,7 +40,7 @@ module.exports =
 /******/ 	// the startup function
 /******/ 	function startup() {
 /******/ 		// Load entry module and return exports
-/******/ 		return __webpack_require__(907);
+/******/ 		return __webpack_require__(104);
 /******/ 	};
 /******/
 /******/ 	// run startup
@@ -56,6 +56,48 @@ module.exports = require("os");
 
 /***/ }),
 
+/***/ 104:
+/***/ (function(__unusedmodule, __unusedexports, __webpack_require__) {
+
+const core = __webpack_require__(470);
+const { execFileSync, execSync } = __webpack_require__(129);
+const fs = __webpack_require__(747);
+
+const hasValue = (input) => {
+  return input.trim().length !== 0;
+};
+
+const github_pat = core.getInput('github_pat');
+
+const actionsList = JSON.parse(core.getInput('actions_list'));
+const basePath = core.getInput('checkout_base_path');
+
+const regex = /^(inmar)\/(.+)@(.+)$/;
+actionsList.forEach((action) => {
+  const match = regex.exec(action);
+  if(match.length === 4) {
+    const owner = match[1];
+    const repo = match[2];
+    const ref = match[3];
+
+    if(owner !== "inmar") {
+      console.log(`Skipping repo ${repo} as it is now owned by inmar.`);
+      return;
+    }
+
+    const cloneUrl = `https://${github_pat}@github.com/${owner}/${repo}.git`
+    const cloneDir = `${basePath}/${repo}`;
+    const cloneCommand = `git clone --depth=1 --single-branch --branch ${ref} ${cloneUrl} ${cloneDir}`;
+
+    console.log(cloneCommand);
+    execSync(cloneCommand);
+  } else {
+    console.log(`The value ${action} does not follow the required format: inmar/repo@ref`);
+  }
+});
+
+/***/ }),
+
 /***/ 129:
 /***/ (function(module) {
 
@@ -63,7 +105,106 @@ module.exports = require("child_process");
 
 /***/ }),
 
-/***/ 207:
+/***/ 431:
+/***/ (function(__unusedmodule, exports, __webpack_require__) {
+
+"use strict";
+
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const os = __importStar(__webpack_require__(87));
+/**
+ * Commands
+ *
+ * Command Format:
+ *   ::name key=value,key=value::message
+ *
+ * Examples:
+ *   ::warning::This is the message
+ *   ::set-env name=MY_VAR::some value
+ */
+function issueCommand(command, properties, message) {
+    const cmd = new Command(command, properties, message);
+    process.stdout.write(cmd.toString() + os.EOL);
+}
+exports.issueCommand = issueCommand;
+function issue(name, message = '') {
+    issueCommand(name, {}, message);
+}
+exports.issue = issue;
+const CMD_STRING = '::';
+class Command {
+    constructor(command, properties, message) {
+        if (!command) {
+            command = 'missing.command';
+        }
+        this.command = command;
+        this.properties = properties;
+        this.message = message;
+    }
+    toString() {
+        let cmdStr = CMD_STRING + this.command;
+        if (this.properties && Object.keys(this.properties).length > 0) {
+            cmdStr += ' ';
+            let first = true;
+            for (const key in this.properties) {
+                if (this.properties.hasOwnProperty(key)) {
+                    const val = this.properties[key];
+                    if (val) {
+                        if (first) {
+                            first = false;
+                        }
+                        else {
+                            cmdStr += ',';
+                        }
+                        cmdStr += `${key}=${escapeProperty(val)}`;
+                    }
+                }
+            }
+        }
+        cmdStr += `${CMD_STRING}${escapeData(this.message)}`;
+        return cmdStr;
+    }
+}
+/**
+ * Sanitizes an input into a string so it can be passed into issueCommand safely
+ * @param input input to sanitize into a string
+ */
+function toCommandValue(input) {
+    if (input === null || input === undefined) {
+        return '';
+    }
+    else if (typeof input === 'string' || input instanceof String) {
+        return input;
+    }
+    return JSON.stringify(input);
+}
+exports.toCommandValue = toCommandValue;
+function escapeData(s) {
+    return toCommandValue(s)
+        .replace(/%/g, '%25')
+        .replace(/\r/g, '%0D')
+        .replace(/\n/g, '%0A');
+}
+function escapeProperty(s) {
+    return toCommandValue(s)
+        .replace(/%/g, '%25')
+        .replace(/\r/g, '%0D')
+        .replace(/\n/g, '%0A')
+        .replace(/:/g, '%3A')
+        .replace(/,/g, '%2C');
+}
+//# sourceMappingURL=command.js.map
+
+/***/ }),
+
+/***/ 470:
 /***/ (function(__unusedmodule, exports, __webpack_require__) {
 
 "use strict";
@@ -85,7 +226,7 @@ var __importStar = (this && this.__importStar) || function (mod) {
     return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const command_1 = __webpack_require__(290);
+const command_1 = __webpack_require__(431);
 const os = __importStar(__webpack_require__(87));
 const path = __importStar(__webpack_require__(622));
 /**
@@ -292,105 +433,6 @@ exports.getState = getState;
 
 /***/ }),
 
-/***/ 290:
-/***/ (function(__unusedmodule, exports, __webpack_require__) {
-
-"use strict";
-
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
-    result["default"] = mod;
-    return result;
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const os = __importStar(__webpack_require__(87));
-/**
- * Commands
- *
- * Command Format:
- *   ::name key=value,key=value::message
- *
- * Examples:
- *   ::warning::This is the message
- *   ::set-env name=MY_VAR::some value
- */
-function issueCommand(command, properties, message) {
-    const cmd = new Command(command, properties, message);
-    process.stdout.write(cmd.toString() + os.EOL);
-}
-exports.issueCommand = issueCommand;
-function issue(name, message = '') {
-    issueCommand(name, {}, message);
-}
-exports.issue = issue;
-const CMD_STRING = '::';
-class Command {
-    constructor(command, properties, message) {
-        if (!command) {
-            command = 'missing.command';
-        }
-        this.command = command;
-        this.properties = properties;
-        this.message = message;
-    }
-    toString() {
-        let cmdStr = CMD_STRING + this.command;
-        if (this.properties && Object.keys(this.properties).length > 0) {
-            cmdStr += ' ';
-            let first = true;
-            for (const key in this.properties) {
-                if (this.properties.hasOwnProperty(key)) {
-                    const val = this.properties[key];
-                    if (val) {
-                        if (first) {
-                            first = false;
-                        }
-                        else {
-                            cmdStr += ',';
-                        }
-                        cmdStr += `${key}=${escapeProperty(val)}`;
-                    }
-                }
-            }
-        }
-        cmdStr += `${CMD_STRING}${escapeData(this.message)}`;
-        return cmdStr;
-    }
-}
-/**
- * Sanitizes an input into a string so it can be passed into issueCommand safely
- * @param input input to sanitize into a string
- */
-function toCommandValue(input) {
-    if (input === null || input === undefined) {
-        return '';
-    }
-    else if (typeof input === 'string' || input instanceof String) {
-        return input;
-    }
-    return JSON.stringify(input);
-}
-exports.toCommandValue = toCommandValue;
-function escapeData(s) {
-    return toCommandValue(s)
-        .replace(/%/g, '%25')
-        .replace(/\r/g, '%0D')
-        .replace(/\n/g, '%0A');
-}
-function escapeProperty(s) {
-    return toCommandValue(s)
-        .replace(/%/g, '%25')
-        .replace(/\r/g, '%0D')
-        .replace(/\n/g, '%0A')
-        .replace(/:/g, '%3A')
-        .replace(/,/g, '%2C');
-}
-//# sourceMappingURL=command.js.map
-
-/***/ }),
-
 /***/ 622:
 /***/ (function(module) {
 
@@ -402,90 +444,6 @@ module.exports = require("path");
 /***/ (function(module) {
 
 module.exports = require("fs");
-
-/***/ }),
-
-/***/ 907:
-/***/ (function(__unusedmodule, __unusedexports, __webpack_require__) {
-
-const core = __webpack_require__(207);
-const { execFileSync, execSync } = __webpack_require__(129);
-const fs = __webpack_require__(747);
-
-const sshHomePath = `${process.env['HOME']}/.ssh`;
-const sshHomeSetup = () => {
-  console.log('Creating SSH home folder');
-  fs.mkdirSync(sshHomePath, { recursive: true });
-  console.log('Configuring known_hosts file');
-  execSync(`ssh-keyscan -H github.com >> ${sshHomePath}/known_hosts`);
-};
-
-const sshAgentStart = () => {
-  console.log('Starting the SSH agent');
-  const sshAgentOutput = execFileSync('ssh-agent');
-  const lines = sshAgentOutput.toString().split('\n');
-  for (const lineNumber in lines) {
-    const matches = /^(SSH_AUTH_SOCK|SSH_AGENT_PID)=(.*); export \1/.exec(lines[lineNumber]);
-    if (matches && matches.length > 0) {
-      process.env[matches[1]] = matches[2];
-    }
-  }
-};
-
-const addPrivateKey = (privateKey) => {
-  console.log('Adding the private key');
-  privateKey.split(/(?=-----BEGIN)/).forEach(function (key) {
-    execSync('ssh-add -', { input: key.trim() + '\n' });
-  });
-  execSync('ssh-add -l', { stdio: 'inherit' });
-};
-
-const sshSetup = (privateKey) => {
-  sshHomeSetup();
-  sshAgentStart();
-  addPrivateKey(privateKey);
-};
-
-const hasValue = (input) => {
-  return input.trim().length !== 0;
-};
-
-const sshPrivateKey = core.getInput('ssh_private_key');
-
-if (hasValue(sshPrivateKey)) {
-  console.log('Setting up the SSH agent with the provided private key');
-  sshSetup(sshPrivateKey);
-} else {
-  console.log('No private key provided. Assuming valid SSH credentials are available');
-}
-
-const actionsList = JSON.parse(core.getInput('actions_list'));
-const basePath = core.getInput('checkout_base_path');
-
-const regex = /^(.+)\/(.+)@(.+)$/;
-actionsList.forEach((action) => {
-  const match = regex.exec(action);
-  if(match.length === 4) {
-    const owner = match[1];
-    const repo = match[2];
-    const ref = match[3];
-
-    const cloneUrl = `git@github.com:${owner}/${repo}.git`;
-    const cloneDir = `${basePath}/${repo}`;
-    const cloneCommand = `git clone --depth=1 --single-branch --branch ${ref} ${cloneUrl} ${cloneDir}`;
-
-    console.log(cloneCommand);
-    execSync(cloneCommand);
-  } else {
-    console.log(`The value ${action} does not follow the required format: owner/repo@ref`);
-  }
-});
-
-if (hasValue(sshPrivateKey)) {
-  console.log('Killing the ssh-agent')
-  execSync('kill ${SSH_AGENT_PID}', { stdio: 'inherit' });
-}
-
 
 /***/ })
 
